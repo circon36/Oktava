8c464d406f1951f239aaf4f1c9e21803d0335d0f
v.shelokov <v.shelokov@mail.ru>
2023-08-27 21:03:15 +0500

    Initial Commit

---
+++ Oktava Watch App/ContentView.swift
@@ -0,0 +1,118 @@
+//
+//  ContentView.swift
+//  Oktava Watch App
+//
+//  Created by Vasily Shelokov on 24.08.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        @State var selectedHour: Int = 0
+        let definition = ["Ш", "С+Ш", "СЗИ"]
+        VStack {
+            HStack{
+                Text("КТ:")
+                    .multilineTextAlignment(.leading)
+                Picker("", selection: $selectedHour) {
+                    ForEach((1...60), id: \.self) {
+                            Text(String($0))
+                            }
+                        }
+                        .pickerStyle(.wheel)
+                        .labelsHidden()
+                Picker("", selection: $selectedHour) {
+                    ForEach(definition, id: \.self) {
+                            Text(String($0))
+                            }
+                        }
+                        .pickerStyle(.wheel)
+                        .labelsHidden()
+            }
+            HStack {
+                Text("125")
+                    .multilineTextAlignment(.leading)
+                Picker("", selection: $selectedHour) {
+                    ForEach((0...100), id: \.self) {
+                            Text(String($0))
+                            }
+                        }
+                        .pickerStyle(.wheel)
+                        .labelsHidden()
+                Spacer()
+                Text("250")
+                Picker("", selection: $selectedHour) {
+                    ForEach((0...100), id: \.self) {
+                            Text(String($0))
+                            }
+                        }
+                        .pickerStyle(.wheel)
+                        .labelsHidden()
+                }
+            HStack {
+                Text("500")
+                    .multilineTextAlignment(.leading)
+                Picker("", selection: $selectedHour) {
+                    ForEach((0...100), id: \.self) {
+                            Text(String($0))
+                            }
+                        }
+                        .pickerStyle(.wheel)
+                        .labelsHidden()
+                Spacer()
+                Text("1000")
+                Picker("", selection: $selectedHour) {
+                    ForEach((0...100), id: \.self) {
+                            Text(String($0))
+                            }
+                        }
+                        .pickerStyle(.wheel)
+                        .labelsHidden()
+            }
+            HStack {
+                Text("2000")
+                    .multilineTextAlignment(.leading)
+                Picker("", selection: $selectedHour) {
+                    ForEach((0...100), id: \.self) {
+                            Text(String($0))
+                            }
+                        }
+                        .pickerStyle(.wheel)
+                        .labelsHidden()
+                Spacer()
+                Text("4000")
+                Picker("", selection: $selectedHour) {
+                    ForEach((0...100), id: \.self) {
+                            Text(String($0))
+                            }
+                        }
+                        .pickerStyle(.wheel)
+                        .labelsHidden()
+            }
+            HStack {
+                Text("8000")
+                    .multilineTextAlignment(.leading)
+                Picker("", selection: $selectedHour) {
+                    ForEach((0...100), id: \.self) {
+                            Text(String($0))
+                            }
+                        }
+                        .pickerStyle(.wheel)
+                        .labelsHidden()
+              
+                }
+            }
+        
+        }
+        
+    
+}
+                
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Oktava Watch App/OktavaApp.swift
@@ -0,0 +1,17 @@
+//
+//  OktavaApp.swift
+//  Oktava Watch App
+//
+//  Created by Vasily Shelokov on 27.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Oktava_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
